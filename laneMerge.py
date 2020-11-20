91ce4324dacf323f667f00674aa5112387695a7a
Anand <anandgok@usc.edu>
2020-11-20 12:46:20 -0800

    Stone Age' Invention of fire- The lines of code that will start every carla

--- laneMerge.py
+++ laneMerge.py
@@ -11,3 +11,26 @@ except IndexError:
     pass
 
 import carla
+
+# These will be the actors whcih will be needed for the list. Used for both init and cleanup
+actor_list = []
+
+try:
+    # We need to ensure the delay factors if using non-local servers, say on another machine dedicated to training.
+    # For non-local machines, delay= sending+training every iter + receivin, all cause delays... use local if possible
+    # NOTE TO SELF: Once final code is set in stone, revisit this and see the delay for the remote machine
+    client = carla.Client("localhost", 2000)
+
+    # Setting 2 sec timeout (the docs use 10)
+    client.set_timeout(2.0)
+
+    # We are loading the World as Town04 since Town04 map is the highway which we are planning to train on
+    # Default: world = client.get_world()
+    world = client.load_world('Town04')
+
+    blueprint_library = world.get_blueprint_library()
+
+finally:
+    for actor in actor_list:
+        actor.destroy()
+    print("All cleaned up!")
