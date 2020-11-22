1ec40af4565a02267d8aa8f6abd5cebb4d4d1a3a
Ashwini Raja <ashwini@usc.edu>
2020-11-22 11:15:33 -0800

    adding modifiedtensorboard

--- RL_Training_Code/Model.py
+++ RL_Training_Code/Model.py
@@ -21,10 +21,47 @@ from threading import Thread
 from Environment import *
 
 
+REPLAY_MEMORY_SIZE = 50_000
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MODEL_NAME = "256x2"
+
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
 class DQNAgent:
     def __init__(self):
         self.model = self.create_model()
-        self.target_model = self.create_model()
+        self.target_model = self.create_model()replay memory size sentdexreplay memory size sentdex
         self.target_model.set_weights(self.model.get_weights())
 
         self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
