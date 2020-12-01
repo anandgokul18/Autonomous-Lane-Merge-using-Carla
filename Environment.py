823af891759c2e3cc7deee82f427c0d39b0ceb5c
Anand <anandgok@usc.edu>
2020-12-01 19:20:38 +0530

    Code cleanup

---
+++ Environment.py
@@ -0,0 +1,266 @@
+import glob
+import os
+import sys
+import random
+import time
+import numpy as np
+import cv2
+import math
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+import carla
+
+from skimage.color import rgb2gray
+
+SHOW_PREVIEW = False
+IM_WIDTH = 300  # 640
+IM_HEIGHT = 240  # 480
+SECONDS_PER_EPISODE = 30  # We need to on-ramp and drive. So increasing to 100
+
+
+class CarEnv:
+    SHOW_CAM = SHOW_PREVIEW
+    STEER_AMT = 1.0
+    im_width = IM_WIDTH
+    im_height = IM_HEIGHT
+    front_camera = None
+
+    # first_lane_change_on_freeway = True
+    lane_crossings = []
+
+    def __init__(self):
+        self.client = carla.Client("127.0.0.1", 2000)
+        self.client.set_timeout(30.0)
+
+        # FIRST do client.load_world(), then, do get_world()
+        # self.world = self.client.get_world()
+        self.world = self.client.load_world('Town04')
+
+        self.blueprint_library = self.world.get_blueprint_library()
+        self.model_3 = self.blueprint_library.filter("model3")[0]
+
+    def reset(self):
+        self.collision_hist = []
+        self.actor_list = []
+
+        # self.first_lane_change_on_freeway = True
+        self.lane_crossings = []
+
+        # desired_spawn_point = carla.Transform(carla.Location(-34.3, -87.5, 2), carla.Rotation(0, 44, 0))
+
+        desired_spawn_point = carla.Transform(
+            carla.Location(230.4, 8.5, 7), carla.Rotation(0, 174, 0))
+
+        # desired_spawn_point = carla.Transform(carla.Location(-34.3, -200.5, 2), carla.Rotation(0, 44, 0))
+
+        self.vehicle = None
+
+        try:
+            self.vehicle = self.world.spawn_actor(
+                self.model_3, desired_spawn_point)
+        except Exception as e:
+            print(f"Error: {e}")
+            time.sleep(20)
+            self.vehicle = self.world.spawn_actor(
+                self.model_3, desired_spawn_point)
+
+        # self.transform = random.choice(self.world.get_map().get_spawn_points())
+        # self.vehicle = self.world.spawn_actor(self.model_3, self.transform)
+        self.actor_list.append(self.vehicle)
+
+        self.rgb_cam = self.blueprint_library.find(
+            'sensor.camera.semantic_segmentation')
+        self.rgb_cam.set_attribute("image_size_x", f"{self.im_width}")
+        self.rgb_cam.set_attribute("image_size_y", f"{self.im_height}")
+        self.rgb_cam.set_attribute("fov", f"75")  # Sentdex: 110
+
+        transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        self.sensor = self.world.spawn_actor(
+            self.rgb_cam, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.sensor)
+        self.sensor.listen(lambda data: self.process_img(data))
+
+        self.vehicle.apply_control(
+            carla.VehicleControl(throttle=0.0, brake=0.0, reverse=False, hand_brake=False))
+        time.sleep(4)
+
+        colsensor = self.blueprint_library.find("sensor.other.collision")
+        self.colsensor = self.world.spawn_actor(
+            colsensor, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.colsensor)
+        self.colsensor.listen(lambda event: self.collision_data(event))
+
+        lanedetectsensor = self.blueprint_library.find(
+            "sensor.other.lane_invasion")  # or sensor.other.lane_invasion
+        self.lanedetectsensor = self.world.spawn_actor(
+            lanedetectsensor, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.lanedetectsensor)
+        self.lanedetectsensor.listen(lambda event: self.lane_crossing(event))
+
+        while self.front_camera is None:
+            time.sleep(0.01)
+
+        self.episode_start = time.time()
+        self.vehicle.apply_control(
+            carla.VehicleControl(throttle=0.0, brake=0.0))
+
+        return self.front_camera
+
+    def collision_data(self, event):
+        self.collision_hist.append(event)
+
+    def lane_crossing(self, event):
+        self.lane_crossings.append(event)
+
+    def process_img(self, image):
+
+        # Semantic segmentation
+        image.convert(carla.ColorConverter.CityScapesPalette)
+
+        i = np.array(image.raw_data)
+        # print(i.shape)
+        i2 = i.reshape((self.im_height, self.im_width, 4))
+        i3 = i2[:, :, :3]
+
+        # grayscale = rgb2gray(i3)
+
+        if self.SHOW_CAM:
+            cv2.imshow("", i3)
+            cv2.waitKey(1)
+        self.front_camera = i3
+
+    def step(self, action):
+
+        # on_ramp = False
+
+        # Starting reward for current step.
+        #reward = 0
+        """
+        current_lane = self.vehicle.get_world().get_map(
+        ).get_waypoint(self.vehicle.get_location())
+        if str(current_lane.left_lane_marking.type) == 'Solid' and str(current_lane.right_lane_marking.type) == 'Solid':
+            print("[LOG] On-ramp is true")
+            on_ramp = True
+        """
+
+        if action == 0:  # full throttle left
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=1.0, steer=-0.25*self.STEER_AMT, reverse=False, hand_brake=False))
+            # print("[LOG] Action 0")
+        elif action == 1:  # full throttle straight
+            self.vehicle.apply_control(
+                carla.VehicleControl(throttle=1.0, steer=0, reverse=False, hand_brake=False))
+            # print("[LOG] Action 1")
+            # reward += 10
+        elif action == 2:  # full throttle right
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=1.0, steer=0.25*self.STEER_AMT, reverse=False, hand_brake=False))
+            # print("[LOG] Action 2")
+        elif action == 3:  # half throttle straight
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=0.5, steer=0, reverse=False, hand_brake=False))
+            # print("[LOG] Action 3")
+            # reward += 5
+        elif action == 4:  # full brake
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=0, steer=0, brake=1, reverse=False, hand_brake=False))
+
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+
+        #done = False
+
+        """
+        current_lane = self.vehicle.get_world().get_map(
+        ).get_waypoint(self.vehicle.get_location())
+        if str(current_lane.left_lane_marking.type) == 'Broken' or str(current_lane.right_lane_marking.type) == 'Broken':
+            on_ramp = False
+            reward += 100
+        """
+
+        """
+        # Going from ramp to freeway
+        new_lane = self.vehicle.get_world().get_map(
+        ).get_waypoint(self.vehicle.get_location())
+        if on_ramp == True and str(new_lane.left_lane_marking.type) == 'Broken' and str(new_lane.right_lane_marking.type) == 'Solid':
+            on_ramp = False
+            reward += 600
+            self.first_lane_change_on_freeway = False
+            print("[LOG] Ramp to freeway!")
+            done = False  # Phase 1 training
+        """
+
+        # Code for penalizing lane changes
+        if len(self.lane_crossings) != 0:
+            for x in self.lane_crossings:
+                clm = x.crossed_lane_markings  # How many events in here?
+                for marking in clm:
+                    # print(str(marking.type))
+                    # str(marking.type) == 'Solid' or str(marking.type) == 'SolidSolid' or str(marking.type) == 'Curb' or str(marking.type) == 'Other':
+                    '''
+                    if str(marking.type) != 'Broken':
+                        reward += -25
+                        print(f"[LOG] {str(marking.type)} Crossed...Penalty")
+                        done = False
+
+                    # Rewarding the first change on the freeway
+                    elif self.first_lane_change_on_freeway == False and str(marking.type) == 'Broken':
+                        reward += 500
+                        self.first_lane_change_on_freeway = True
+                        print("[LOG] First lane change on freeway")
+                        done = False
+
+                    # Rewarding the merging as one single operation
+                    if on_ramp and str(marking.type) == 'Broken':
+                        reward += 500
+                        on_ramp = False
+                        print("[LOG] Merge successful")
+                        done = False
+                    # else: # Penalizing unnecessary lane changes
+                    #    reward += 0 #-10 #Phase 1 ...not penalizing broken changes
+                        # print("[LOG] Lane Change penalty")
+                    '''
+
+                    #print(f"[LOG] {str(marking.type)} Crossed...Penalty")
+                    #reward += -2
+                    #reward += 0
+                    pass
+
+        if len(self.collision_hist) != 0:
+            done = True
+            print("[LOG] Collided...Done")
+            """
+            if on_ramp:
+                reward += -1000  # penalizing heavily to stop q values from doing only right on onramp
+            else:
+                reward += -200
+            """
+            reward = -200
+        elif kmh < 50:
+            done = False
+            reward = 1
+        else:
+            done = False
+            reward = 4
+
+        """
+        # Survival reward and speed check
+        if not done:
+            reward += 2  # +2 for each survival step
+
+            if kmh < 50:
+                reward += -1
+            elif kmh >= 50:
+                reward += 1
+        """
+
+        if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            done = True
+
+        return self.front_camera, reward, done, None
---
+++ Model.py
@@ -0,0 +1,259 @@
+import glob
+import os
+import sys
+import random
+import time
+import numpy as np
+import cv2
+import math
+import matplotlib.pyplot as plt
+from collections import deque
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Sequential, Model
+from keras.layers import Dense, GlobalAveragePooling2D, Input, Concatenate, Conv2D, AveragePooling2D, Activation, Flatten
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+import keras.backend.tensorflow_backend as backend
+from tensorflow.python.keras.backend import set_session
+from tensorflow.python.keras.models import load_model
+from threading import Thread
+from Environment import *
+
+
+REPLAY_MEMORY_SIZE = 50_000
+MIN_REPLAY_MEMORY_SIZE = 10_000
+MINIBATCH_SIZE = 64  # How many steps to use for training
+PREDICTION_BATCH_SIZE = 1
+TRAINING_BATCH_SIZE = MINIBATCH_SIZE // 4
+UPDATE_TARGET_EVERY = 5
+MODEL_NAME = "Xception"
+
+MEMORY_FRACTION = 0.75
+MIN_REWARD = -100
+
+EPISODES = 100000
+
+DISCOUNT = 0.9
+epsilon = 1
+EPSILON_DECAY = 0.95  # 0.9975 99975
+MIN_EPSILON = 0.001
+
+AGGREGATE_STATS_EVERY = 10
+#backend._SYMBOLIC_SCOPE.value = True
+
+IM_WIDTH = 300  # 640
+IM_HEIGHT = 240  # 480
+# 1 is single channel for grayscale, use 3 for rgb
+INPUT_SHAPE = (IM_HEIGHT, IM_WIDTH, 3)
+
+# Own Tensorboard class
+
+
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+
+class DQNAgent:
+    def __init__(self, loadExistingModel=None):
+
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(
+            log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
+        self.target_update_counter = 0
+
+        # Adding it to instance, becaause sess is used in train_in_loop() as well
+        self.loadExistingModel = loadExistingModel
+        if self.loadExistingModel:
+
+            # adding session Ashwini
+            self.sess = tf.Session().__enter__()
+            self.graph = tf.compat.v1.get_default_graph()
+            set_session(self.sess)
+
+            self.model = self.load_model(loadExistingModel)
+            self.target_model = self.load_model(loadExistingModel)
+
+        else:
+
+            self.graph = tf.compat.v1.get_default_graph()
+
+            self.model = self.create_model()
+            self.target_model = self.create_model()
+
+        self.target_model.set_weights(self.model.get_weights())
+        self.terminate = False
+        self.last_logged_episode = 0
+        self.training_initialized = False
+
+    def load_model(self, path):
+        model = tf.keras.models.load_model(path)
+        return model
+
+    def create_model(self):
+
+        # Anand: default weights was None. Using "imagenet" weights
+        base_model = Xception(weights="imagenet", include_top=False,
+                              input_shape=INPUT_SHAPE)
+
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+
+        predictions = Dense(3, activation="linear")(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        model.compile(loss="mse", optimizer=Adam(
+            lr=0.001), metrics=["accuracy"])
+        return model
+
+    """
+    def create_model(self):
+
+        input = Input(shape=INPUT_SHAPE)
+
+        cnn_1 = Conv2D(64, (7, 7), padding='same')(input)
+        cnn_1a = Activation('relu')(cnn_1)
+        cnn_1c = Concatenate()([cnn_1a, input])
+        cnn_1ap = AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same')(cnn_1c)
+
+        cnn_2 = Conv2D(64, (5, 5), padding='same')(cnn_1ap)
+        cnn_2a = Activation('relu')(cnn_2)
+        cnn_2c = Concatenate()([cnn_2a, cnn_1ap])
+        cnn_2ap = AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same')(cnn_2c)
+
+        cnn_3 = Conv2D(128, (5, 5), padding='same')(cnn_2ap)
+        cnn_3a = Activation('relu')(cnn_3)
+        cnn_3c = Concatenate()([cnn_3a, cnn_2ap])
+        cnn_3ap = AveragePooling2D(pool_size=(5, 5), strides=(2, 2), padding='same')(cnn_3c)
+
+        cnn_4 = Conv2D(256, (5, 5), padding='same')(cnn_3ap)
+        cnn_4a = Activation('relu')(cnn_4)
+        cnn_4c = Concatenate()([cnn_4a, cnn_3ap])
+        cnn_4ap = AveragePooling2D(pool_size=(5, 5), strides=(2, 2), padding='same')(cnn_4c)
+
+        cnn_5 = Conv2D(512, (3, 3), padding='same')(cnn_4ap)
+        cnn_5a = Activation('relu')(cnn_5)
+        #cnn_5c = Concatenate()([cnn_5a, cnn_4ap])
+        cnn_5ap = AveragePooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(cnn_5a)
+
+        flatten = Flatten()(cnn_5ap)
+
+        #return input, flatten
+
+        predictions = Dense(3, activation="linear")(flatten) #grayscale. Dimensionality=1 or 3 for rgb
+        model = Model(inputs=input, outputs=predictions)
+        model.compile(loss="mse", optimizer=Adam(
+            lr=0.001), metrics=["accuracy"])
+        return model
+    """
+
+    def update_replay_memory(self, transition):
+        # transition = (current_state, action, reward, new_state, done)
+        self.replay_memory.append(transition)
+
+    def train(self):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        current_states = np.array([transition[0]
+                                   for transition in minibatch])/255
+        with self.graph.as_default():
+            set_sess(self.sess)
+            current_qs_list = self.model.predict(
+                current_states, PREDICTION_BATCH_SIZE)
+
+        new_current_states = np.array(
+            [transition[3] for transition in minibatch])/255
+        with self.graph.as_default():
+            set_sess(self.sess)
+            future_qs_list = self.target_model.predict(
+                new_current_states, PREDICTION_BATCH_SIZE)
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        log_this_step = False
+        if self.tensorboard.step > self.last_logged_episode:
+            log_this_step = True
+            self.last_log_episode = self.tensorboard.step
+
+        #X = rgb2gray(X)
+
+        with self.graph.as_default():
+            self.model.fit(np.array(X)/255, np.array(y), batch_size=TRAINING_BATCH_SIZE,
+                           verbose=0, shuffle=False, callbacks=[self.tensorboard] if log_this_step else None)
+
+        if log_this_step:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+    def train_in_loop(self):
+        X = np.random.uniform(size=(1, IM_HEIGHT, IM_WIDTH, 3)).astype(
+            np.float32)  # grayscale index 3 is 1 for grayscale and 3 for rgb
+        y = np.random.uniform(size=(1, 3)).astype(
+            np.float32)  # grayscale (1,1), rgb (1,3)
+        with self.graph.as_default():
+
+            if self.loadExistingModel:
+                set_session(self.sess)
+
+            self.model.fit(X, y, verbose=False, batch_size=1)
+
+        self.training_initialized = True
+
+        while True:
+            if self.terminate:
+                return
+            self.train()
+            time.sleep(0.01)
